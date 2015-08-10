3d0cc15e644e235e24cabe4790e3cbf014256de4
atmqq1990 <atmqq1990@gmail.com>
2015-08-10 08:30:23 +0800

    personal solving

---
+++ JzOffer/跳台阶.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+	int jumpFloor(int number) {
+		if (number <= 1)
+			return 1;
+		else return jumpFloor(number - 1) + jumpFloor(number - 2);
+
+	}
+};
+
+
+int main(int argc, char** args)
+{
+	Solution so;
+	int res = so.jumpFloor(4);
+	cout << res << endl;
+
+	system("pause");
+	return 0;
+}
